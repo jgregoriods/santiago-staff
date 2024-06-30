2dc39a1eae3090c3222df1973e89d7b8370eaddb
jgregoriods <jonas.gregorio@gmail.com>
2024-06-30 09:21:05 +0200

    add main file

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from horley_encoding import convert_to_horley
+from nltk.collocations import BigramAssocMeasures, BigramCollocationFinder, TrigramAssocMeasures, TrigramCollocationFinder
+from nltk.lm.preprocessing import pad_both_ends
+from sklearn.feature_extraction.text import TfidfVectorizer
+from cosine_cost import CosineCost, plot_breakpoints
 
 
 def load_file(file_path):
@@ -53,12 +57,52 @@ def split_sequences(lines):
     return sequences
 
 
+def bigram_collocations(sequences):
+    padded = [list(pad_both_ends(sequence, 2)) for sequence in sequences]
+    bigram_measures = BigramAssocMeasures()
+
+    finder = BigramCollocationFinder.from_documents(padded)
+    finder.apply_freq_filter(2)
+    finder.apply_ngram_filter(lambda *w: w[1] != '<76>' or w[0] == '?')
+    print(finder.score_ngrams(bigram_measures.likelihood_ratio)[:10])
+
+    finder = BigramCollocationFinder.from_documents(padded)
+    finder.apply_freq_filter(2)
+    finder.apply_ngram_filter(lambda *w: w[0] != '<76>')
+    print(finder.score_ngrams(bigram_measures.likelihood_ratio)[:10])
+
+    finder = BigramCollocationFinder.from_documents(padded)
+    finder.apply_freq_filter(2)
+    finder.apply_ngram_filter(lambda *w: w[-1] != '</s>')
+    print(finder.score_ngrams(bigram_measures.likelihood_ratio)[:10])
+
+
+def trigram_collocations(sequences):
+    padded = [list(pad_both_ends(sequence, 3)) for sequence in sequences]
+    trigram_measures = TrigramAssocMeasures()
+
+    finder = TrigramCollocationFinder.from_documents(padded)
+    finder.apply_freq_filter(2)
+    finder.apply_ngram_filter(lambda *w: w[1] != '<76>' or w[0] == '?')
+    print(finder.score_ngrams(trigram_measures.likelihood_ratio)[:10])
+
+
+def vectorize(lines):
+    line_str = [' '.join(line) for line in lines]
+    vectorizer = TfidfVectorizer(analyzer="word", token_pattern = '[0-9]+[a-zAZ]*[.0-9]*[a-zAZ]*')
+    vectorized_text = vectorizer.fit_transform(line_str)
+    return vectorized_text
+
+
 def main():
     raw_data_I = load_file('data/I.csv')
     clean_data_I = clean_lines(raw_data_I)
     encoded_data_I = encode_lines(clean_data_I)
     sequences_I = split_sequences(encoded_data_I)
-    print(sequences_I)
+    bigram_collocations(sequences_I)
+    trigram_collocations(sequences_I)
+    vectorized_text_I = vectorize(encoded_data_I)
+    bkpts = plot_breakpoints(vectorized_text_I, [1, 2])
 
 
 if __name__ == '__main__':
